5f0e17a937dd0c537e88d208b4a09eab27121f12
Musfiquer Rhman <musfiquerrhman@gmail.com>
2021-09-11 19:41:12 +0600

    create react app, add palate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Pallate from './pallate';
+import seedcolors from './seedcolors';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pallate palette={ seedcolors[4] }/>
     </div>
   );
 }
